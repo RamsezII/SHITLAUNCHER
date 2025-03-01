a7377ec9ad287e5b0707e285215648e6260861f0
ram16 <thomass.montaine@gmail.com>
2025-03-01 21:40:16 +0100

    Remove noconsole option from PyInstaller commands in build scripts

--- generate_executable.ps1
+++ generate_executable.ps1
@@ -1,9 +1,7 @@
-# Activer l'environnement virtuel
+
 . .\.venv\Scripts\Activate
 
-# Exécuter PyInstaller
 pyinstaller --onefile --icon=icon.ico --noconsole --name=SHITLAUNCHER_noconsole .\ShitLauncher.py
 pyinstaller --onefile --icon=icon.ico --name=SHITLAUNCHER .\ShitLauncher.py
 
-# Désactiver l'environnement virtuel
 deactivate
