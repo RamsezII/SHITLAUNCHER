aba4f86dce47179db92bc2fb1fb8e27d15e8d44b
ram16 <thomass.montaine@gmail.com>
2025-03-01 18:44:21 +0100

    Add debug spec file and update PowerShell script for console and non-console executables

--- generate_executable.ps1
+++ generate_executable.ps1
@@ -2,7 +2,8 @@
 . .\.venv\Scripts\Activate
 
 # Exécuter PyInstaller
-pyinstaller --onefile --icon=icon.ico --name=SHITLAUNCHER .\ShitLauncher.py
+pyinstaller --onefile --icon=icon.ico --noconsole --name=SHITLAUNCHER .\ShitLauncher.py
+pyinstaller --onefile --icon=icon.ico --name=SHITLAUNCHER_console .\ShitLauncher.py
 
 # Désactiver l'environnement virtuel
 deactivate
