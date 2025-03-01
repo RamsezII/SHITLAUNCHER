5eac925e775fba190279cfe57689b6982426b08d
ram16 <thomass.montaine@gmail.com>
2025-03-01 22:35:46 +0100

    Add traceback to error handling in ShitLauncher main function

--- ShitLauncher.py
+++ ShitLauncher.py
@@ -78,5 +78,6 @@ if __name__ == "__main__":
         main()
     except Exception as e:
         print(f"Error: {e}")
+        print(e.with_traceback())
         input("Press Enter to exit")
         sys.throw(e)
