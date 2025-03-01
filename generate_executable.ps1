1e2e1e7d6932b190ccb19f071d4d44d6e2079730
ram16 <thomass.montaine@gmail.com>
2025-03-01 20:13:18 +0100

    Update executable generation scripts to create a noconsole version of ShitLauncher

--- generate_executable.ps1
+++ generate_executable.ps1
@@ -2,8 +2,8 @@
 . .\.venv\Scripts\Activate
 
 # Exécuter PyInstaller
-pyinstaller --onefile --icon=icon.ico --noconsole --name=SHITLAUNCHER .\ShitLauncher.py
-pyinstaller --onefile --icon=icon.ico --name=SHITLAUNCHER_prompt .\ShitLauncher.py
+pyinstaller --onefile --icon=icon.ico --noconsole --name=SHITLAUNCHER_noconsole .\ShitLauncher.py
+pyinstaller --onefile --icon=icon.ico --name=SHITLAUNCHER .\ShitLauncher.py
 
 # Désactiver l'environnement virtuel
 deactivate
--- generate_executable.sh
+++ generate_executable.sh
@@ -4,8 +4,8 @@
 source .venv/bin/activate
 
 # Exécuter pyinstaller
-pyinstaller.exe --onefile --icon=icon.ico --noconsole --name=SHITLAUNCHER ./ShitLauncher.py
-pyinstaller.exe --onefile --icon=icon.ico --name=SHITLAUNCHER_prompt ./ShitLauncher.py
+pyinstaller.exe --onefile --icon=icon.ico --noconsole --name=SHITLAUNCHER_noconsole ./ShitLauncher.py
+pyinstaller.exe --onefile --icon=icon.ico --name=SHITLAUNCHER ./ShitLauncher.py
 
 # Désactiver l'environnement virtuel
 deactivate
