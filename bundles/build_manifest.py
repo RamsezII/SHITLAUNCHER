8ceffe4754b1d1aef7efc8bae91f25cca43393c1
ram16 <thomass.montaine@gmail.com>
2025-03-04 21:55:32 +0100

    Implement bundle extraction and manifest generation; create temporary directory for asset handling

--- bundles/build_manifest.py
+++ bundles/build_manifest.py
@@ -1,5 +1,7 @@
 import json
 import os
+import zipfile
+import shutil
 
 
 path_this = os.path.abspath(__file__)
@@ -7,6 +9,11 @@ root_dir = os.path.dirname(path_this)
 
 
 if __name__ == "__main__":
+
+    temp_folder = os.path.join(os.path.dirname(root_dir), "_TEMP_")
+    if not os.path.exists(temp_folder):
+        os.makedirs(temp_folder)
+
     manifest = {}
 
     for bundle_name in os.listdir(root_dir):
@@ -16,8 +23,16 @@ if __name__ == "__main__":
             print('bundle: ', bundle_name)
             manifest_bundle = {}
 
-            for asset_name in os.listdir(bundle_path):
-                asset_path = os.path.join(bundle_path, asset_name)
+            zip_path = os.path.join(bundle_path, "texts.zip")
+            extract_dir = os.path.join(temp_folder, bundle_name)
+            if not os.path.exists(extract_dir):
+                os.makedirs(extract_dir)
+
+            with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+                zip_ref.extractall(extract_dir)
+
+            for asset_name in os.listdir(extract_dir):
+                asset_path = os.path.join(extract_dir, asset_name)
 
                 if os.path.isdir(asset_path):
                     print('\tasset: ' + asset_name)
@@ -31,6 +46,8 @@ if __name__ == "__main__":
                     manifest_bundle[asset_name] = manifest_asset
 
             manifest[bundle_name] = manifest_bundle
+
+    shutil.rmtree(temp_folder)
 
     json_path = os.path.join(root_dir, "manifest.json.txt")
     with open(json_path, "w", encoding="utf-8") as json_file:
