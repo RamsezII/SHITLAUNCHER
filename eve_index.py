824f1d8f814794afb11e763504f8a710e85a9763
ram16 <thomass.montaine@gmail.com>
2025-04-15 15:44:23 +0200

    Add initial implementation of directory indexing and JSON export in eve_index.py

---
+++ eve_index.py
@@ -0,0 +1,23 @@
+import os, json
+
+def index_recursive(path):
+    entries = []
+    for entry in os.scandir(path):
+        if entry.is_dir():
+            entries.append({
+                "name": entry.name,
+                "type": "directory",
+                "children": index_recursive(entry.path)
+            })
+        else:
+            entries.append({
+                "name": entry.name,
+                "type": "file"
+            })
+    return entries
+
+base_path = "/var/www/paragon/texts"
+index = index_recursive(base_path)
+
+with open("/var/www/paragon/eve_index.json", "w") as f:
+    json.dump(index, f, indent=2)
